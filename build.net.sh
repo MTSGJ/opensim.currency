14ec2948a798c6fb6d644626719e60054956b0c8
root <fumi.hax@gmail.com>
2025-04-05 20:38:48 +0900

    net9.0 or net8.0

--- build.net.sh
+++ build.net.sh
@@ -2,22 +2,22 @@
 
 CONFIGPATH=./config
 OPNSIMPATH=../bin
-DOTNETVER=6.0
+DOTNETVER=9
 
 echo "==================="
 echo "DTL/NSL_CURRENCY"
 echo "==================="
 
 ./clean.sh
-./runprebuild.net.sh
+./runprebuild.net.sh $DOTNETVER
 dotnet build -c Release OpenSim.Currency.sln || exit 1
 
 echo
-cp -f bin/net${DOTNETVER}/OpenSim.Data.MySQL.MySQLMoneyDataWrapper.dll $OPNSIMPATH
-cp -f bin/net${DOTNETVER}/OpenSim.Modules.Currency.dll $OPNSIMPATH
-cp -f bin/net${DOTNETVER}/MoneyServer.dll $OPNSIMPATH
-cp -f bin/net${DOTNETVER}/MoneyServer $OPNSIMPATH
-cp -f bin/net${DOTNETVER}/MoneyServer.runtimeconfig.json $OPNSIMPATH
+cp -f bin/net${DOTNETVER}.0/OpenSim.Data.MySQL.MySQLMoneyDataWrapper.dll $OPNSIMPATH
+cp -f bin/net${DOTNETVER}.0/OpenSim.Modules.Currency.dll $OPNSIMPATH
+cp -f bin/net${DOTNETVER}.0/MoneyServer.dll $OPNSIMPATH
+cp -f bin/net${DOTNETVER}.0/MoneyServer $OPNSIMPATH
+cp -f bin/net${DOTNETVER}.0/MoneyServer.runtimeconfig.json $OPNSIMPATH
 
 #
 rm -f $OPNSIMPATH/OpenSim.Forge.Currency.dll
--- runprebuild.net.sh
+++ runprebuild.net.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
+DOTNETVER=8
+
+if [ "$1" != "" ]; then
+    DOTNETVER=$1
+fi
+
 echo y | dotnet ../bin/prebuild.dll /file prebuild.net.xml /clean
 
-dotnet ../bin/prebuild.dll /target vs2022 /targetframework net6_0 /excludedir = "obj | bin" /file prebuild.net.xml
+dotnet ../bin/prebuild.dll /target vs2022 /targetframework net${DOTNETVER}_0 /excludedir = "obj | bin" /file prebuild.net.xml
 
